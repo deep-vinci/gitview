3d07a20fc255ffbadef38ee8c39e107494a663a8
Deepak <deepakworkpc@gmail.com>
2025-06-04 03:00:37 +0530

    chore: inital commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import SearchBar from "./ui/search-bar";
+
+export default function Home() {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+}
