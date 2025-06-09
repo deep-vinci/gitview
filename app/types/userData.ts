629aa92eab0f5e80a40cd4eac76bf974ac29b4bc
Deepak <deepakworkpc@gmail.com>
2025-06-10 00:11:08 +0530

    feat: types

---
+++ app/types/userData.ts
@@ -0,0 +1,62 @@
+export interface UserData {
+    name: string;
+    login: string;
+    avatarUrl: string;
+    bio: string;
+    company: string;
+    location: string;
+    websiteUrl: string;
+    twitterUsername: string;
+    followers: {
+        totalCount: number;
+    };
+    following: {
+        totalCount: number;
+    };
+    contributionsCollection: {
+        contributionCalendar: {
+            totalContributions: number;
+            weeks: Array<{
+                contributionDays: Array<{
+                    contributionCount: number;
+                    date: string;
+                    weekday: number;
+                    color: string;
+                }>;
+            }>;
+        };
+        commitContributionsByRepository: Array<{
+            repository: {
+                nameWithOwner: string;
+                url: string;
+            };
+            contributions: {
+                totalCount: number;
+            };
+        }>;
+        issueContributions: {
+            totalCount: number;
+        };
+        pullRequestContributions: {
+            totalCount: number;
+        };
+        pullRequestReviewContributions: {
+            totalCount: number;
+        };
+        restrictedContributionsCount: number;
+    };
+    repositories: {
+        nodes: Array<{
+            name: string;
+            description: string;
+            url: string;
+            stargazerCount: number;
+            forkCount: number;
+            updatedAt: string;
+            primaryLanguage: {
+                name: string;
+                color: string;
+            } | null; // primaryLanguage can be null
+        }>;
+    };
+}
