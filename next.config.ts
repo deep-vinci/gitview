d8a494de78499b6d0855be3b659f5a716370b83e
Deepak <deepakworkpc@gmail.com>
2025-06-09 23:47:42 +0530

    chore: test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        remotePatterns: [
+            new URL("https://avatars.githubusercontent.com/u/1206493?v=4"),
+        ],
+    },
 };
 
 export default nextConfig;
