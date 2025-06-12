adf3b7bff420fc3ace2317d2f06a431159303f0c
Deepak <deepakworkpc@gmail.com>
2025-06-13 03:41:22 +0530

    fix: .

--- app/[user]/layout.tsx
+++ app/[user]/layout.tsx
@@ -5,12 +5,18 @@ import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
 import SideBar from "../components/sidebar";
 // import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
     return (
         <UserDataProvider>
-            <div className="flex">
-                <div className="w-full">{children}</div>
-                <SideBar />
+            <div className="flex flex-col md:flex-row ">
+                <div className="w-full h-[50dvh] md:h-[100dvh]">{children}</div>
+                <div className="w-full h-[50dvh] overflow-y-auto md:w-100 md:h-[100dvh] md:">
+                    <SideBar />
+                </div>
             </div>
         </UserDataProvider>
     );
