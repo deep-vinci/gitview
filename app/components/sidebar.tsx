09cfb502e4ad8a09378cad5b2f6e1a9494eac767
Deepak <deepakworkpc@gmail.com>
2025-06-13 01:42:16 +0530

    feat: add responsiveness to sidebar
    add a coming soon button to widgets, split sidebar component into smaller components

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -2,14 +2,7 @@
 
 import { useState } from "react";
 import { Button } from "./ui/button";
-import {
-    Card,
-    CardAction,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "./ui/card";
+// import { Card, CardDescription } from "./ui/card";
 import { Input } from "./ui/input";
 import { ModeToggle } from "./ui/modeToggle";
 import Link from "next/link";
@@ -17,10 +10,15 @@ import Form from "next/form";
 import { Share } from "./share";
 import { useModel } from "../context/modelProvider";
 import Repositories from "./repositories";
+import Image from "next/image";
+import { Card, CardDescription, CardHeader } from "./ui/card";
+import { useUserData } from "../context/UserDataContext";
 
-export default function SideBar({ username }: { username: String }) {
+export default function SideBar() {
     const [userInput, setUserInput] = useState("");
     const { model, setModel } = useModel();
+    const { userData, fetchData } = useUserData();
+
     // w-100 h-[100vh] flex flex-col
     return (
         <div className="h-full flex flex-col">
@@ -29,7 +27,7 @@ export default function SideBar({ username }: { username: String }) {
                 <ModeToggle />
                 {/* <Button className="ml-auto mr-0">Share</Button> */}
                 <div className="ml-auto mr-0">
-                    <Share username={username} />
+                    <Share username={userData?.login} />
                 </div>
             </div>
 
@@ -54,20 +52,31 @@ export default function SideBar({ username }: { username: String }) {
             {/* repo */}
             <p className="pl-3 text-sm font-bold">Repositories</p>
             <div className="mask-b-from-70% w-full h-200 gap-3 scroll-my-1.5 p-3 flex flex-col md:overflow-auto md:relative">
-                <Repositories />
+                <Repositories username={userData?.login} />
             </div>
 
-            <div className="w-full p-3 flex flex-col gap-3 mt-auto">
-                <p className="text-sm font-bold">Widget</p>
-                <Card>
+            <div className="w-full p-3 flex flex-col  gap-3 mt-auto">
+                <p className="text-sm font-bold">Github Widget</p>
+                <Card className="p-0 relative">
                     <CardHeader>
-                        <CardTitle>Card Title</CardTitle>
-                        <CardDescription>Card Description</CardDescription>
-                        <CardAction>Card Action</CardAction>
+                        {/* <CardDescription>Github</CardDescription> */}
+                        <div className="flex justify-center items-center mr-10  p-0">
+                            {" "}
+                            <Image
+                                className="m-0"
+                                src={"/widget.png"}
+                                height={40}
+                                width={140}
+                                alt="sad"
+                            />
+                        </div>
+                        <Button
+                            variant={"secondary"}
+                            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                        >
+                            Coming soon
+                        </Button>
                     </CardHeader>
-                    <CardContent>
-                        <p>Card Content</p>
-                    </CardContent>
                 </Card>
             </div>
 
