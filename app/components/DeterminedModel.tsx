13bfb38a859bc71a0689faa155fd62ad27229bfe
Deepak <deepakworkpc@gmail.com>
2025-06-15 23:51:44 +0530

    refactor: broke down the mega component into smaller stats component

--- app/components/DeterminedModel.tsx
+++ app/components/DeterminedModel.tsx
@@ -13,6 +13,7 @@ import { Card, CardTitle } from "./ui/card";
 import Image from "next/image";
 import { isMobile } from "react-device-detect";
 import { useState } from "react";
+import Stats from "./stats";
 
 export default function DeterminedModel({ username }: { username: String }) {
     const { model, setModel } = useModel();
@@ -22,41 +23,7 @@ export default function DeterminedModel({ username }: { username: String }) {
     return (
         <div className="w-full h-full relative">
             <div className="absolute bottom-0 left-0">
-                {!isMobile ? (
-                    <Card className="p-5 m-5">
-                        <div className="space-y-1">
-                            <h4 className="text-sm font-semibold">
-                                Commit Streak
-                            </h4>
-                            <div className="flex gap-1 items-center ">
-                                <Image
-                                    src={"/streak.png"}
-                                    alt="streaks"
-                                    height={20}
-                                    width={25}
-                                />
-                                <p className="text-sm">
-                                    Current streak 12 days
-                                </p>
-                            </div>
-                            <div className="flex gap-1 items-center ">
-                                <Image
-                                    src={"/best.png"}
-                                    alt="streaks"
-                                    height={20}
-                                    width={25}
-                                />
-                                <p className="text-sm">
-                                    Highest streak 12 days
-                                </p>
-                            </div>
-
-                            <div className="text-muted-foreground text-xs">
-                                Joined December 2021
-                            </div>
-                        </div>{" "}
-                    </Card>
-                ) : null}
+                {!isMobile ? <Stats /> : null}
             </div>
             {model == "3d" ? <Graph username={username} /> : <Heatmap />}
         </div>
