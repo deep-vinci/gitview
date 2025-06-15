8a83ba3978d1d078e2101f870239092eb163d52a
Deepak <deepakworkpc@gmail.com>
2025-06-16 00:02:52 +0530

    fix: readble dates

--- app/components/stats.tsx
+++ app/components/stats.tsx
@@ -37,38 +37,42 @@ export default function Stats() {
 
         setStreakInfo({ longestStreak, currentStreak });
     }, [userData]);
+    if (userData)
+        return (
+            <Card className="p-5 m-5">
+                <div className="space-y-1">
+                    <h4 className="text-sm font-semibold">Commit Streak</h4>
+                    <div className="flex gap-1 items-center ">
+                        <Image
+                            src={"/streak.png"}
+                            alt="streaks"
+                            height={20}
+                            width={25}
+                        />
+                        <p className="text-sm">
+                            Current streak {streakInfo?.currentStreak} days
+                        </p>
+                    </div>
+                    <div className="flex gap-1 items-center ">
+                        <Image
+                            src={"/best.png"}
+                            alt="streaks"
+                            height={20}
+                            width={25}
+                        />
+                        <p className="text-sm">
+                            Highest streak {streakInfo?.longestStreak} days
+                        </p>
+                    </div>
 
-    return (
-        <Card className="p-5 m-5">
-            <div className="space-y-1">
-                <h4 className="text-sm font-semibold">Commit Streak</h4>
-                <div className="flex gap-1 items-center ">
-                    <Image
-                        src={"/streak.png"}
-                        alt="streaks"
-                        height={20}
-                        width={25}
-                    />
-                    <p className="text-sm">
-                        Current streak {streakInfo?.currentStreak} days
-                    </p>
+                    <div className="text-muted-foreground text-xs">
+                        Joined {new Date(userData.createdAt).getFullYear()}{" "}
+                        {new Date(userData.createdAt).toLocaleString(
+                            "default",
+                            { month: "long" }
+                        )}
+                    </div>
                 </div>
-                <div className="flex gap-1 items-center ">
-                    <Image
-                        src={"/best.png"}
-                        alt="streaks"
-                        height={20}
-                        width={25}
-                    />
-                    <p className="text-sm">
-                        Highest streak {streakInfo?.longestStreak} days
-                    </p>
-                </div>
-
-                <div className="text-muted-foreground text-xs">
-                    Joined {userData?.createdAt}
-                </div>
-            </div>{" "}
-        </Card>
-    );
+            </Card>
+        );
 }
