683132c12107d4afe0bb8ced3a74be064b22e673
Deepak <deepakworkpc@gmail.com>
2025-06-15 23:55:26 +0530

    chore: don't know why i had two instance of same interface

--- app/context/UserDataContext.tsx
+++ app/context/UserDataContext.tsx
@@ -8,68 +8,7 @@ import {
     useState,
 } from "react";
 
-interface UserData {
-    name: string;
-    login: string;
-    avatarUrl: string;
-    bio: string;
-    company: string;
-    location: string;
-    websiteUrl: string;
-    twitterUsername: string;
-    followers: {
-        totalCount: number;
-    };
-    following: {
-        totalCount: number;
-    };
-    contributionsCollection: {
-        contributionCalendar: {
-            totalContributions: number;
-            weeks: Array<{
-                contributionDays: Array<{
-                    contributionCount: number;
-                    date: string;
-                    weekday: number;
-                    color: string;
-                }>;
-            }>;
-        };
-        commitContributionsByRepository: Array<{
-            repository: {
-                nameWithOwner: string;
-                url: string;
-            };
-            contributions: {
-                totalCount: number;
-            };
-        }>;
-        issueContributions: {
-            totalCount: number;
-        };
-        pullRequestContributions: {
-            totalCount: number;
-        };
-        pullRequestReviewContributions: {
-            totalCount: number;
-        };
-        restrictedContributionsCount: number;
-    };
-    repositories: {
-        nodes: Array<{
-            name: string;
-            description: string;
-            url: string;
-            stargazerCount: number;
-            forkCount: number;
-            updatedAt: string;
-            primaryLanguage: {
-                name: string;
-                color: string;
-            } | null; // primaryLanguage can be null
-        }>;
-    };
-}
+import { UserData } from "../types/userData";
 
 interface UserDataProvider {
     children: ReactNode;
