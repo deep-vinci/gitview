76752a4bd2002326e75cf202a112f08306d74c81
Deepak <deepakworkpc@gmail.com>
2025-06-24 08:35:26 +0530

    fix: center the model choosing button on small screen

--- app/components/switchModel.tsx
+++ app/components/switchModel.tsx
@@ -8,7 +8,7 @@ export default function SwitchModel() {
 
     return (
         <div className="absolute left-1/2 transform -translate-x-1/2 mt-5 z-10">
-            <Tabs defaultValue="account" className="w-[400px]">
+            <Tabs defaultValue="account" className="">
                 <TabsList>
                     <TabsTrigger onClick={() => setModel("3d")} value="account">
                         3d map
